6374f6c247dda0a325d65d7e409e3089be7ccd9a
Lev Ekasov <lev.ekasov@gmail.com>
2020-03-25 13:45:16 +0100

    maven publsij

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
+    id("org.jetbrains.kotlin.jvm")
+    `java-library`
     id("pl.allegro.tech.build.axion-release") version "1.11.0"
+    `maven-publish`
 }
 scmVersion {
     useHighestVersion = true
@@ -8,3 +11,17 @@ scmVersion {
 }
 
 project.version = scmVersion.version
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,29 +12,17 @@ plugins {
     `java-library`
     `maven-publish`
 }
+
 publishing {
     repositories {
         mavenLocal()
     }
 }
 
-
-repositories {
-    // Use jcenter for resolving dependencies.
-    // You can declare any Maven/Ivy/file repository here.
-    jcenter()
-}
-
-dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+allprojects {
+    repositories {
+        // Use jcenter for resolving dependencies.
+        // You can declare any Maven/Ivy/file repository here.
+        jcenter()
+    }
 }
